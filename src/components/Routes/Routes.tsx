6c2f773171e6da7c7cf505d9be286bcc66e9b3e8
Alexandra Vargas <alexa1866@gmail.com>
2023-05-23 11:34:35 +0200

    Create hello world page and scene

--- src/components/Routes/Routes.tsx
+++ src/components/Routes/Routes.tsx
@@ -3,6 +3,7 @@ import { Redirect, Route, Switch } from 'react-router-dom';
 import { HomePage } from '../../pages/Home';
 import { PageWithTabs } from '../../pages/WithTabs';
 import { WithDrilldown } from '../../pages/WithDrilldown';
+import { HelloWorld } from '../../pages/HelloWorld';
 import { prefixRoute } from '../../utils/utils.routing';
 import { ROUTES } from '../../constants';
 
@@ -12,6 +13,7 @@ export const Routes = () => {
       <Route path={prefixRoute(`${ROUTES.WithTabs}`)} component={PageWithTabs} />
       <Route path={prefixRoute(`${ROUTES.WithDrilldown}`)} component={WithDrilldown} />
       <Route path={prefixRoute(`${ROUTES.Home}`)} component={HomePage} />
+      <Route path={prefixRoute(`${ROUTES.HelloWorld}`)} component={HelloWorld} />
       <Redirect to={prefixRoute(ROUTES.Home)} />
     </Switch>
   );
